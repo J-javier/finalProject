f34902193d22e50f64e86286958bfe790b34203d
unknown <jose360pelico@gmail.com>
2023-06-29 23:09:23 -0600

    second commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Main from "@/components/Home/home"
+
+export default function Home() {
+  return (
+    <>
+      <Main/> 
+    </>
+  )
+}
---
+++ components/Home/home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+/* import "@/components/Home/home.css" */
+import "./home.css"
+
+function home() {
+  return (
+    <div className='mainContainer'>
+        <section className='sectionOne'>
+            <div className='firstSection'>
+                <button className='button'>Search for places</button>
+                <div className='location'>
+                    <img src="./location.svg" alt="Location" />
+                </div>
+            </div>
+            <div className='imagen'>
+                <img src="Shower.png" alt="weather image" />
+            </div> 
+        </section>
+        {/* Section number Two */}
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+
+    </div>
+  )
+}
+
+export default home
