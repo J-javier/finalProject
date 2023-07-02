6842f29f79087215d5c91fdc5241792caa905b19
unknown <jose360pelico@gmail.com>
2023-07-02 00:22:41 -0600

    new styles were added

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Main from "@/components/Home/home"
 import Card from '@/components/card/Card'
+import CardTwo from '@/components/cardDetail/CardTwo'
 
 export default function Home() {
   return (
     <>
       <Main/> 
       <Card/>
+      <CardTwo/>
     </>
   )
 }
--- components/cardDetail/CardTwo.jsx
+++ components/cardDetail/CardTwo.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
+import "./CardTwo.css"
 
 function CardTwo() {
   return (
-    <div>
-        
-    </div>
+    <>
+        <div className='contenerdorPrincipal'>
+            {/* First Section */}
+            <section>
+                <p className='subTitle'>Wind Status</p>
+                <div className='medida'>
+                    <h1 className='numero'>7</h1>
+                    <h3 className='tipoMedicion'>mph</h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="./row.svg" alt="navegation" />
+                    </div>
+                    <p className='tipoMedicion'>WSW</p>
+                </div>
+            </section>
+            {/* Second Section */}
+            <section>
+                <p className='subTitle'>Humidity</p>
+                    <div>
+                        <h1 className='numero'>84%</h1>
+                        {/* <h3>mph</h3> */}
+                    </div>
+                    <div>
+                        <div>
+                            <div></div>
+                        </div>
+                        
+                    </div>    
+            </section>
+            {/* Third Section */}
+            <section>
+                <p className='subTitle'>VISIBILITY</p>
+                <div className='medida'>
+                    <h1 className='numero'>6,4</h1>
+                    <p className='tipoMedicion'>MILES</p>
+                </div>
+            </section>
+            {/* Four Section */}
+            <section>
+                <p className='subTitle'>Air Pressure</p>
+                <div className='medida'>
+                    <h1 className='numero'>998</h1>
+                    <p className='tipoMedicion'>mb</p>
+                </div>
+            </section>            
+        </div>
+    </>
   )
 }
 
