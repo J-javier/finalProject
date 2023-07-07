3e81cf4928d480a26375e75627f9078686ae1f2c
unknown <jose360pelico@gmail.com>
2023-07-07 01:23:48 -0600

    new features were added

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <>
+        <nav>
+            
+
+
+        </nav>  
+    </>
+  )
+}
+
+export default NavBar
